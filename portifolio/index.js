0e3b57054c85f70b04188264b626945637d48426
Joberth Rogers <joberth.rogers18@gmail.com>
2019-12-21 15:53:16 -0400

    Added more competences Added minor fixes
    Signed-off-by: Joberth rogers <joberth.rogers18@gmail.com>

--- portifolio/index.js
+++ portifolio/index.js
@@ -3,8 +3,8 @@
 
 var html_comp = document.querySelectorAll(".item")
 
-percent_array = ["70%", "80%", "90%", "90%", "75%", "85%", "70%", "50%", "40%"];
-color_array = ["#2aabde", "#f0662b", "#f0bd26", "#007ab3", "#e82a30", "#4eb5ab", "#f24e29", "#3cde82", "#3cde82"];
+percent_array = ["70%", "80%", "90%", "90%", "75%", "85%", "70%", "50%", "55%", "40%", "50%", "70%"];
+color_array = ["#2aabde", "#f0662b", "#f0bd26", "#007ab3", "#e82a30", "#4eb5ab", "#f24e29", "#3cde82", "#3cde82", "#007ab3", "#b21f66", "#589636"];
 
 html_comp.forEach((competence, index) => {
 
