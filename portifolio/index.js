d4f7aabe0ec9aebfc54d1d2e06b3d95f6f44c751
Joberth Rogers <joberth.rogers18@gmail.com>
2019-12-24 22:11:05 -0400

    Fixed the main responsive screen mode
    Signed-off-by: Joberth Rogers <joberth.rogers18@gmail.com>

--- portifolio/index.js
+++ portifolio/index.js
@@ -1,6 +1,9 @@
 
 // competences hover
 
+console.log(window.innerHeight);
+console.log(window.innerWidth);
+
 var html_comp = document.querySelectorAll(".item")
 
 percent_array = ["70%", "80%", "90%", "90%", "75%", "85%", "70%", "50%", "55%", "40%", "50%", "70%"];
@@ -8,18 +11,21 @@ color_array = ["#2aabde", "#f0662b", "#f0bd26", "#007ab3", "#e82a30", "#4eb5ab",
 
 html_comp.forEach((competence, index) => {
 
-    competence.addEventListener("mouseover", function() {
-        var images = competence.querySelectorAll('i');
-        images[0].style.display = "none";
-        images[1].style.display = "inline";
-        let line = competence.querySelector('.line');
-        line.style.width = percent_array[index];
-        line.style.backgroundColor = color_array[index];
-    });
+    if (window.innerWidth > 400 ) {
+        competence.addEventListener("mouseover", function() {
+            var images = competence.querySelectorAll('i');
+            images[0].style.display = "none";
+            images[1].style.display = "inline";
+            let line = competence.querySelector('.line');
+            line.style.width = percent_array[index];
+            line.style.backgroundColor = color_array[index];
+        });
+    
+        competence.addEventListener("mouseout", function() {
+            var images = competence.querySelectorAll('i');
+            images[1].style.display = "none";
+            images[0].style.display = "inline";
+        });
+    }
 
-    competence.addEventListener("mouseout", function() {
-        var images = competence.querySelectorAll('i');
-        images[1].style.display = "none";
-        images[0].style.display = "inline";
-    });
 })
